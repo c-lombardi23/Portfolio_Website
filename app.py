4491d09a8c1fb24541aabddedec0e21d073e81f8
Christopher Lombardi <clombardi@thorlabs.com>
2025-08-05 09:33:31 -0400

    many style changes

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ def index():
 def projects():
     return 'Project Page'
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/focal")
+def focal():
+    return render_template('focal.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
 if __name__ == "__main__":
 
     app.run(debug=True)
